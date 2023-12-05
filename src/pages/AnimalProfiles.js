3683c6ac5103a6a9806451d828644c5ef2291c17
ceonz <ceon91@gmail.com>
2023-12-05 12:26:37 -0500

    project initialization

--- src/pages/AnimalProfiles.js
+++ src/pages/AnimalProfiles.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import '../App.css';
-import './Home';
-
+import "./Home";
 
 function AnimalProfiles() {
-    return (
-        <div>
-            <h1> Animal Profiles </h1>
-            <div >
-                <a href="/Home">
-                    <button>
-                        Home Page
-                    </button>
-                </a>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1> Animal Profiles </h1>
+      <div>
+        <Link to="./home">
+          <button>Home Page</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default AnimalProfiles;
