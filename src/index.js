c263fe1c233a5658d47e6363e1e456b312c2104b
ceonz <45135014+ceonz@users.noreply.github.com>
2023-12-05 12:38:26 -0500

    project initialization (#3)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
 import Home from './pages/Home';
 import AnimalProfiles from './pages/AnimalProfiles';
 import './index.css';
@@ -12,16 +10,11 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Home' element={<Home />} />
-          <Route path='/AnimalProfiles' element={<AnimalProfiles />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/animal-profiles" element={<AnimalProfiles />} />
       </Routes>
     </Router>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import '../App.css';
-import './AnimalProfiles';
+import "./AnimalProfiles";
 
 function Home() {
-    return (
-        <div>
-            <h1> Animal Shelter Management System </h1>
-            <div >
-                <a href="/AnimalProfiles">
-                    <button>
-                        Animal Profiles
-                    </button>
-                </a>
-            </div>
-            <div>
-                <button>
-                    Shelter Tasks
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1> Animal Shelter Management System </h1>
+      <div>
+        <Link to="./animal-profiles">
+          <button>Animal Profiles</button>
+        </Link>
+      </div>
+      <div>
+        <button>Shelter Tasks</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
-
