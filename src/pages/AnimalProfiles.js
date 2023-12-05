a153f5e1623c4041e50ef4f721082c147054127c
Jason Chen 24079491 <73327854+jchen7381@users.noreply.github.com>
2023-12-05 16:10:37 -0500

    added animal search with user text input

--- src/pages/AnimalProfiles.js
+++ src/pages/AnimalProfiles.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import TextInput from "./TextInput";
 import { Link } from "react-router-dom";
 import "./Home";
 
 function AnimalProfiles() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchInputChange = (text) => {
+    setSearchText(text);
+  };
+
+  const animals = [
+    { name: "Tom", species: "Feline", age: 2 },
+    { name: "Jerry", species: "Mouse", age: 3 },
+  
+  ];
+
+  const filteredAnimals = animals.filter((animal) =>
+    animal.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <div>
-      <h1> Animal Profiles </h1>
+      <h1>Animal Profiles</h1>
+      <TextInput onInputChange={handleSearchInputChange} />
+      <ul>
+        {filteredAnimals.map((animal, index) => (
+          <li key={index}>
+            <Link to={`/animal/${animal.name}`}>{animal.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
