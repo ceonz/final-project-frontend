736a8c645fb0ff22a76ed6b147a25f30530fb6c0
Ellahi <ellahinator02@gmail.com>
2023-12-05 13:05:40 -0500

    add navbar

--- src/pages/AnimalProfiles.js
+++ src/pages/AnimalProfiles.js
@@ -6,11 +6,6 @@ function AnimalProfiles() {
   return (
     <div>
       <h1> Animal Profiles </h1>
-      <div>
-        <Link to="/home">
-          <button>Home Page</button>
-        </Link>
-      </div>
     </div>
   );
 }
