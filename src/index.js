044f655e108c77e27763e2c03c2e3199f9b5787f
abarikdar <73364852+abarikdar@users.noreply.github.com>
2023-12-04 20:57:49 -0500

    add home page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import Home from "./pages/Home";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Home' element={<Home />} />
+      </Routes>
+    </Router>
     <App />
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+
+function Home() {
+    return (
+        <div>
+            <h1> Animal Shelter Management System </h1>
+            <div >
+                <button>
+                    Animal Profiles
+                </button>
+                <button>
+                    Shelter Tasks
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
+
