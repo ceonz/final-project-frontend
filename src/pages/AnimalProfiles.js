dac4e3a8d5e8957816d56784ba14ab360d437936
Jason Chen 24079491 <73327854+jchen7381@users.noreply.github.com>
2023-12-05 17:10:00 -0500

    added animal search with user text input

--- src/pages/AnimalProfiles.js
+++ src/pages/AnimalProfiles.js
@@ -1,23 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAnimals } from '../store/actions/animalActions';
+import TextInput from './TextInput';
 
 function AnimalProfiles() {
   const dispatch = useDispatch();
   const { data: animals, loading, error } = useSelector(state => state.animals);
 
+  const [searchText, setSearchText] = useState("");
+
   useEffect(() => {
     dispatch(fetchAnimals());
   }, [dispatch]);
 
+  const handleSearchInputChange = (text) => {
+    setSearchText(text);
+    
+  };
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
+  const filteredAnimals = animals.filter(animal =>
+    animal.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <div>
-      <h1>Animal Profiles</h1>
+      <h1> Animal Profiles </h1>
+      <TextInput onInputChange={handleSearchInputChange} />
       <ul>
-        {animals.map(animal => (
+        {filteredAnimals.map(animal => (
           <li key={animal.id}>{animal.name} - {animal.species}</li>
         ))}
       </ul>
