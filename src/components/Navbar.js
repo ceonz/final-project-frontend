dc5ac2e0866e0037af68e61fe9562b260cbcfe94
Ellahi <87584757+Ellahinator@users.noreply.github.com>
2023-12-18 16:01:24 -0500

    update navbar style + change font to inter (#16)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import PawPrintIcon from './PawPrintIcon';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/home">Home</Link>
-      <Link to="/animal-profiles">Animal Profiles</Link>
-      <Link to="/shelter-tasks">Shelter Tasks</Link>
-      <Link to="/register-animal">Register Animal</Link>
-    </nav>
+    <header className={styles.header}>
+      <NavLink to="/" className={styles.logoLink}>
+        <PawPrintIcon className={styles.pawIcon} />
+        <span className={styles.srOnly}>Animal Shelter</span>
+      </NavLink>
+      <div className={styles.navLinksContainer}> 
+        <NavLink to="/home" className={styles.navLink}>Home</NavLink>
+        <NavLink to="/animal-profiles" className={styles.navLink}>Animal Profiles</NavLink>
+        <NavLink to="/shelter-tasks" className={styles.navLink}>Shelter Tasks</NavLink>
+        <NavLink to="/register-animal" className={styles.navLink}>Register Animal</NavLink>
+      </div>
+    </header>
   );
 }
 
